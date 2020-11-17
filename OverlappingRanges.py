4f9b0469c8642512021ded1bab0ec52a2b829774
peri-ram <74618166+peri-ram@users.noreply.github.com>
2020-11-17 13:54:39 -0500

    Update OverlappingRanges.py

--- OverlappingRanges.py
+++ OverlappingRanges.py
@@ -1,19 +1,15 @@
-class FirstList(list):
-    def __lt__(self, other):
-        return self[0] < other[0]
-
 class Solution:
     
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        h = list( FirstList(x) for x in intervals)
+        h = list( (x[0], x) for x in intervals)
         heapq.heapify(h)
         print(h)
         
         ret = list()
-        curr = heapq.heappop(h) if(h) else None
+        (_, curr) = heapq.heappop(h) if(h) else None
 
         while(h):
-            x = heapq.heappop(h)
+            (_, x) = heapq.heappop(h)
             #print("CURR", curr, "POP", x)
             if(x[0] <= curr[1]):
                 curr[1] = max(curr[1], x[1])
